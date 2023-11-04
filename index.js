140ad981fb26dbe4f069fb95d0c4923852a89421
fai-sas <craftedbyfaisal@gmail.com>
2023-11-04 20:22:24 +0600

    end point created for getting single room by id

--- index.js
+++ index.js
@@ -42,6 +42,13 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/api/v1/get-rooms/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await hotelBookingCollection.findOne(query)
+      res.send(result)
+    })
+
     //  Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
     console.log(
