e395b4737798c6248ecc53cc8b0e331ba3e7abc5
fai-sas <craftedbyfaisal@gmail.com>
2023-11-04 18:36:00 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -10,7 +10,33 @@ app.use(express.json())
 
 const port = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@node-express.sczsc.mongodb.net/?retryWrites=true&w=majority`
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+})
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect()
+    //  Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 })
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    )
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close()
+  }
+}
+run().catch(console.dir)
+
+app.get('/api/v1', (req, res) => {
   res.send('hotel booking app is running...')
 })
 
